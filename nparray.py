0ae81003d244de8d6ddf66957376b308ac5ec6c7
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-06-06 21:53:33 +0900

    [feat] matrix, tensor의 기본 정의 구현

--- nparray.py
+++ nparray.py
@@ -8,3 +8,14 @@ print(test_array)
 print(type(test_array))
 print(test_array.dtype) #데이터 타입을 반환한다. float64
 print(test_array.shape) #배열의 shape를 반환한다. (4, )
+
+matrix = np.array([[1,4,5,8],[1,4,5,8],[1,4,5,8]], int) #matrix(행렬)을 만든 상태에서
+print(matrix.dtype) #데이터 타입을 반환한다. int64
+print(matrix.shape) #배열의 모양을 반환한다. 2차원 배열의 형태 (3,4)를 반환한다.
+
+tensor = np.array([[[1, 4, 5, 8], [1, 4, 5, 8], [1, 4, 5, 8]],
+                    [[1,4,5,8],[1,4,5,8],[1,4,5,8]],
+                    [[1,4,5,8],[1,4,5,8],[1,4,5,8]]], int) #tensor(3차원)을 만든 상태에서
+print(tensor.dtype) #데이터 타입을 반환한다. int64
+print(tensor.shape) #배열의 모양을 반환한다. 3차원 배열의 형태 (3, 3, 4)를 반환한다. 첫 값은 깊이
+
