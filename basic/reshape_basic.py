7ab9acbcc886789ba367d28f17942032b65a723c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-06-06 22:00:47 +0900

    [feat] 모르는 array의 길이가 있을 때 이에 맞게 reshape해주는 코드 실습

--- basic/reshape_basic.py
+++ basic/reshape_basic.py
@@ -5,5 +5,14 @@ tensor = np.array(matrix).reshape(3, 2, 2)
 print(tensor)
 print(np.array(tensor).shape)
 print(np.array(tensor).dtype)
+print('----------------')
 
 # reshape에서 dimension에 맞게 배열을 초기화 시켜준다. size가 같아야 동작하며, 다르면 에러가 터진다.
+
+
+# tensor = np.array(matrix).reshape(4, 2, 2) 에러가 발생한다. 길이가 맞지 않는다.
+# 4대신에 -1을 넣으면 뒤의 값들을 기반으로 배열을 맞춰준다.
+tensor = np.array(matrix).reshape(-1, 2, 2)
+print(tensor)
+print(np.array(tensor).shape)
+print(np.array(tensor).dtype)
